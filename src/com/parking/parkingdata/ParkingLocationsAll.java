8f2b82fe9382c670eb98fd12515275ea6385ef3d
Mandar Mulherkar <mandar.mulherkar@gmail.com>
2012-03-15 23:34:33 -0700

    basic data

--- src/com/parking/parkingdata/ParkingLocationsAll.java
+++ src/com/parking/parkingdata/ParkingLocationsAll.java
@@ -1,5 +1,10 @@
 package com.parking.parkingdata;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class ParkingLocationsAll {
 
+   private List<ParkingLocationDataEntry> parkingLocations = new LinkedList<ParkingLocationDataEntry>();
+   
 }

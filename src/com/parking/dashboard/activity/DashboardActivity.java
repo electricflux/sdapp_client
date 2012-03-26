98a5050e0d9061a1df1e068b532b1a3402c37918
Prasana <angolkarprasana+sdapp@gmail.com>
2012-03-25 22:38:51 -0700

    Prasna:: adding dbHelper class, creating db onCreate and adding db in assets folder

--- src/com/parking/dashboard/activity/DashboardActivity.java
+++ src/com/parking/dashboard/activity/DashboardActivity.java
@@ -1,9 +1,14 @@
 
 package com.parking.dashboard.activity;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.SQLException;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -12,11 +17,15 @@ import com.parking.findparking.FindParkingTabs;
 import com.parking.locatemycar.LocateMyCar;
 import com.parking.payforspot.PayForSpot;
 import com.parking.paymenthistory.PaymentHistory;
+import com.parking.dbManager.DataBaseHelper;
 
 public class DashboardActivity extends Activity{
    
    
-   @Override
+   public static final String TAG = "DashboardActivity";
+   public static Context myContext = null;
+
+@Override
    protected void onCreate(Bundle savedInstanceState) {
       // TODO Auto-generated method stub
       super.onCreate(savedInstanceState);
@@ -29,7 +38,41 @@ public class DashboardActivity extends Activity{
       findViewById(R.id.dashboard_button_manage).setOnClickListener(dBClickListener);
       findViewById(R.id.dashboard_button_personalbests).setOnClickListener(dBClickListener);
       
+      DashboardActivity.myContext = getApplicationContext();
+      
+		Log.v(TAG, "Create DB: 1 ");
+
+		DataBaseHelper myDbHelper = null;
+		myDbHelper = new DataBaseHelper(null);
+		myDbHelper = new DataBaseHelper(getAppContext());
+
+		try {
+
+			myDbHelper.createDataBase();
+
+		} catch (IOException ioe) {
+
+			throw new Error("Unable to create database");
+
+		}
+
+		try {
+
+			myDbHelper.openDataBase();
+			myDbHelper.dbquery(1);
+			myDbHelper.close();
+
+		} catch (SQLException sqle) {
+
+			throw sqle;
+
+		}
+      
    }
+
+	public static Context getAppContext() {
+	    return DashboardActivity.myContext;
+	}
 
    private class DashboardClickListener implements OnClickListener {
       @Override
---
+++ src/com/parking/dbManager/DataBaseHelper.java
@@ -0,0 +1,204 @@
+package com.parking.dbManager;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataBaseHelper extends SQLiteOpenHelper{
+	 
+    private static final String TAG = "DataBaseHelper";
+
+	//The Android's default system path of your application database.
+    private static String DB_PATH = "/data/data/com.parking.dashboard/databases/";
+    private static String DB_NAME = "parking_info.db";
+    
+    private SQLiteDatabase myDataBase; 
+    private final Context myContext;
+ 
+    /**
+     * Constructor
+     * Takes and keeps a reference of the passed context in order to access to the application assets and resources.
+     * @param context
+     */
+    public DataBaseHelper(Context context) {
+ 
+    	super(context, DB_NAME, null, 1);
+        this.myContext = context;
+        if (context != null)
+        	Log.v(TAG, "DataBaseHelper::1 context not null");
+        //Log.v(TAG, "DataBaseHelper::2");
+    }	
+ 
+  /**
+     * Creates a empty database on the system and rewrites it with your own database.
+     * */
+    public void createDataBase() throws IOException{
+ 
+    	//Log.v(TAG, "createDataBase::1 ");
+    	boolean dbExist = checkDataBase();
+ 
+    	if(dbExist){
+    		//do nothing - database already exist
+    		Log.v(TAG, "createDataBase::2 do nothing - database already exist");
+    	}else{
+ 
+    		//By calling this method and empty database will be created into the default system path
+               //of your application so we are gonna be able to overwrite that database with our database.
+        	this.getReadableDatabase();
+        	//Log.v(TAG, "createDataBase 3 ");
+ 
+        	try {
+ 
+        		//Log.v(TAG, "createDataBase 4 ");
+    			copyDataBase();
+    			//Log.v(TAG, "createDataBase 5 ");
+ 
+    		} catch (IOException e) {
+        		throw new Error("Error copying database");
+        	}
+    	}
+ 
+    }
+ 
+    /**
+     * Check if the database already exist to avoid re-copying the file each time you open the application.
+     * @return true if it exists, false if it doesn't
+     */
+    private boolean checkDataBase(){
+ 
+    	SQLiteDatabase checkDB = null;
+ 
+    	try{
+    		String myPath = DB_PATH + DB_NAME;
+    		checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+    		Log.v(TAG, "checkDataBase::0 ");
+ 
+    	}catch(SQLiteException e){
+    		//database does't exist yet.
+    		Log.v(TAG, "checkDataBase:: 1 database does't exist yet.");
+    	}
+ 
+    	if(checkDB != null){
+    		Log.v(TAG, "checkDataBase NOT NULL " + checkDB.getPath());
+    		checkDB.close();
+    	}
+ 
+    	//Log.v(TAG, "checkDataBase::2 ");
+    	return checkDB != null ? true : false;
+    }
+ 
+    /**
+     * Copies your database from your local assets-folder to the just created empty database in the
+     * system folder, from where it can be accessed and handled.
+     * This is done by transfering bytestream.
+     * */
+    private void copyDataBase() throws IOException{
+    	//Open your local db as the input stream
+		Log.v(TAG, "copydatabase:: 1");
+	    InputStream myinput = null;
+		try {
+			myinput = myContext.getAssets().open(DB_NAME);
+		} catch (IOException    e1) {
+			// TODO Auto-generated catch block
+			Log.v(TAG, "copydatabase error 0");
+			e1.printStackTrace();
+		}
+
+	    //Log.v(TAG, "copydatabase 2");
+	    // Path to the just created empty db
+	    String outfilename = DB_PATH + DB_NAME;
+
+	    //Log.v(TAG, "copydatabase 3");
+	    //Open the empty db as the output stream
+	    OutputStream myoutput = null;
+		try {
+			myoutput = new FileOutputStream(outfilename.toString());
+		} catch (FileNotFoundException  e) {
+			// TODO Auto-generated catch block
+			Log.v(TAG, "copydatabase error 1");
+			e.printStackTrace();
+		}
+
+	    // transfer byte to inputfile to outputfile
+	    byte[] buffer = new byte[1024];
+	    int length;
+	    while ((length = myinput.read(buffer))>0)
+	    {
+	        myoutput.write(buffer,0,length);
+	    }
+
+	    //Close the streams
+	    myoutput.flush();
+	    myoutput.close();
+	    myinput.close();
+ 
+    }
+ 
+    public void openDataBase() throws SQLException{
+ 
+    	//Open the database
+    	Log.v(TAG, "openDataBase::1 ");
+        String myPath = DB_PATH + DB_NAME;
+    	myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+    	//Log.v(TAG, "openDataBase 2 ");
+    	
+    	if (myDataBase != null)
+    		Log.v(TAG, "openDataBase NOT NULL ");
+    		
+ 
+    }
+ 
+    @Override
+	public synchronized void close() {
+ 
+    	Log.v(TAG, "close 1");
+    	    if(myDataBase != null)
+    		    myDataBase.close();
+ 
+    	    super.close();
+ 
+	}
+ 
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+ 
+	}
+ 
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+ 
+	}
+	
+	public void dbquery(int i){
+		Log.v(TAG, "dbquery 0 ");
+		Cursor cursor = myDataBase.query("parking_info", null, null, null, null, null, null);
+		if (cursor != null)
+		{
+			Log.v(TAG, "dbquery 1 " + cursor.getColumnCount() + cursor.getCount());
+		/*	Log.v(TAG, "dbquery 1 " + cursor.getInt(1));
+			Log.v(TAG, "dbquery 1 " + cursor.getString(2));
+			Log.v(TAG, "dbquery 1 " + cursor.getString(3));
+			Log.v(TAG, "dbquery 1 " + cursor.getFloat(4));
+			Log.v(TAG, "dbquery 1 " + cursor.getFloat(5));
+			
+			*/
+		}
+		
+	}
+ 
+        // Add your public helper methods to access and get content from the database.
+       // You could return cursors by doing "return myDataBase.query(....)" so it'd be easy
+       // to you to create adapters for your views.
+ 
+}
+

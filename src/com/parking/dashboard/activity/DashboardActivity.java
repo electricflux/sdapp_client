92284bf7c5a3b89563df511a8e12980971f63e7d
jayarama <jayarama@qualcomm.com>
2012-04-03 01:21:31 -0700

    Compilation error resolve

--- src/com/parking/dashboard/activity/DashboardActivity.java
+++ src/com/parking/dashboard/activity/DashboardActivity.java
@@ -25,86 +25,85 @@ import com.parking.dbManager.DataBaseHelper;
 public class DashboardActivity extends Activity{
 	private static final String TAG = DashboardActivity.class.getSimpleName();
 
-	public static final String TAG = "DashboardActivity";
-	   public static Context myContext = null;
-	   public static DataBaseHelper myDbHelper = null;
+	public static Context myContext = null;
+	public static DataBaseHelper myDbHelper = null;
 
 	@Override
-	   protected void onCreate(Bundle savedInstanceState) {
-	      // TODO Auto-generated method stub
-	      super.onCreate(savedInstanceState);
-	      setContentView(R.layout.dashboard);
-	      
-	    //attach event handler to dash buttons
-	      DashboardClickListener dBClickListener = new DashboardClickListener();
-	      findViewById(R.id.dashboard_button_find_parking).setOnClickListener(dBClickListener);
-	      findViewById(R.id.dashboard_button_viewall).setOnClickListener(dBClickListener);
-	      findViewById(R.id.dashboard_button_manage).setOnClickListener(dBClickListener);
-	      findViewById(R.id.dashboard_button_personalbests).setOnClickListener(dBClickListener);
-	      
-	      DashboardActivity.myContext = getApplicationContext();
-	      
-			Log.v(TAG, "Create DB: 1 ");
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dashboard);
 
-			myDbHelper = new DataBaseHelper(null);
-			myDbHelper = new DataBaseHelper(getAppContext());
+		//attach event handler to dash buttons
+		DashboardClickListener dBClickListener = new DashboardClickListener();
+		findViewById(R.id.dashboard_button_find_parking).setOnClickListener(dBClickListener);
+		findViewById(R.id.dashboard_button_viewall).setOnClickListener(dBClickListener);
+		findViewById(R.id.dashboard_button_manage).setOnClickListener(dBClickListener);
+		findViewById(R.id.dashboard_button_personalbests).setOnClickListener(dBClickListener);
 
-			try {
+		DashboardActivity.myContext = getApplicationContext();
 
-				myDbHelper.createDataBase();
+		Log.v(TAG, "Create DB: 1 ");
 
-			} catch (IOException ioe) {
+		myDbHelper = new DataBaseHelper(null);
+		myDbHelper = new DataBaseHelper(getAppContext());
 
-				throw new Error("Unable to create database");
+		try {
 
-			}
+			myDbHelper.createDataBase();
 
-			try {
+		} catch (IOException ioe) {
 
-				myDbHelper.openDataBase();
-				myDbHelper.dbquery(1);
-				myDbHelper.close();
+			throw new Error("Unable to create database");
 
-			} catch (SQLException sqle) {
-
-				throw sqle;
-
-			}
-	      
-	   }
-
-		public static Context getAppContext() {
-		    return DashboardActivity.myContext;
 		}
 
-	   private class DashboardClickListener implements OnClickListener {
-	      @Override
-	      public void onClick(View v) {
-	          Intent i = null;
-	          switch (v.getId()) {
-	              case R.id.dashboard_button_find_parking:
-	                  i = new Intent(DashboardActivity.this, FindParkingTabs.class);
-	                  ParkingLocationsAll mParkingLocationsAll = new ParkingLocationsAll();
-	                  mParkingLocationsAll.getParkingLocations(10, myDbHelper);
-	                  break;
-	              case R.id.dashboard_button_viewall:
-	                  i = new Intent(DashboardActivity.this, PayForSpot.class);
-	                  break;
-	              case R.id.dashboard_button_manage:
-	                  i = new Intent(DashboardActivity.this, PaymentHistory.class);
-	                  break;
-	              case R.id.dashboard_button_personalbests:
-	                  i = new Intent(DashboardActivity.this, LocateMyCar.class);
-	                  break;
-	              default:
-	                  break;
-	          }
-	          if(i != null) {
-	              startActivity(i);
-	          }
-	      }
-	  }
-	   
+		try {
+
+			myDbHelper.openDataBase();
+			myDbHelper.dbquery(1);
+			myDbHelper.close();
+
+		} catch (SQLException sqle) {
+
+			throw sqle;
+
+		}
+
+	}
+
+	public static Context getAppContext() {
+		return DashboardActivity.myContext;
+	}
+
+	private class DashboardClickListener implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			Intent i = null;
+			switch (v.getId()) {
+			case R.id.dashboard_button_find_parking:
+				i = new Intent(DashboardActivity.this, FindParkingTabs.class);
+				ParkingLocationsAll mParkingLocationsAll = new ParkingLocationsAll();
+				mParkingLocationsAll.getParkingLocations(10, myDbHelper);
+				break;
+			case R.id.dashboard_button_viewall:
+				i = new Intent(DashboardActivity.this, PayForSpot.class);
+				break;
+			case R.id.dashboard_button_manage:
+				i = new Intent(DashboardActivity.this, PaymentHistory.class);
+				break;
+			case R.id.dashboard_button_personalbests:
+				i = new Intent(DashboardActivity.this, LocateMyCar.class);
+				break;
+			default:
+				break;
+			}
+			if(i != null) {
+				startActivity(i);
+			}
+		}
+	}
+
 
 	@Override
 	protected void onResume()

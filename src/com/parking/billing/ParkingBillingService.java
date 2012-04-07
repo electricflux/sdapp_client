0eb54284c2d852d0b150cee01ac69feaec514cd1
Mandar Mulherkar <mandar.mulherkar@gmail.com>
2012-04-07 11:18:48 -0700

    some basic empty files for billing

---
+++ src/com/parking/billing/ParkingBillingService.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2011 QUALCOMM Incorporated. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * QUALCOMM Incorporated ("Proprietary Information"). You shall not
+ * disclose such Proprietary Information, and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with QUALCOMM Incorporated.
+ */
+package com.parking.billing;
+
+public class ParkingBillingService {
+
+}

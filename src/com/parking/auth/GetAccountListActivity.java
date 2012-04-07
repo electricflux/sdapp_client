dfc17ab0b106c0ce1c2cf39f2984e2ea2e9eaa8e
jayarama <jayarama@qualcomm.com>
2012-04-07 14:56:44 -0700

    Aesthetic changes for the authenticator

--- src/com/parking/auth/GetAccountListActivity.java
+++ src/com/parking/auth/GetAccountListActivity.java
@@ -8,22 +8,32 @@ import android.accounts.AccountManager;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class GetAccountListActivity extends ListActivity {
 	protected AccountManager accountManager;
 	protected Intent intent;
-
+	
 	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		accountManager = AccountManager.get(getApplicationContext());
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account_list);
+ 
+        //add header to list
+        ListView lv = getListView();
+        LayoutInflater inflater = getLayoutInflater();
+        View header = inflater.inflate(R.layout.account_list_header, (ViewGroup) findViewById(R.id.header_layout_root));
+        lv.addHeaderView(header, null, false);
+ 
+        accountManager = AccountManager.get(getApplicationContext());
 		Account[] accounts = accountManager.getAccountsByType("com.google");
-		this.setListAdapter(new ArrayAdapter(this, R.layout.listitem, accounts));        
-	}
+		this.setListAdapter(new ArrayAdapter(this, R.layout.listitem, accounts)); 
+    }
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {

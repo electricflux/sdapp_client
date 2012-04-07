a514d3c1d0655db27f9dd294acdccd8b59063400
Mandar Mulherkar <mandar.mulherkar@gmail.com>
2012-04-07 15:15:02 -0700

    billing changes

--- src/com/parking/findparking/ParkingSpotAndPaymentInformation.java
+++ src/com/parking/findparking/ParkingSpotAndPaymentInformation.java
@@ -1,17 +1,27 @@
 package com.parking.findparking;
 
-import com.parking.dashboard.R;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parking.billing.BillingConstants.ResponseCode;
+import com.parking.billing.ParkingBillingService;
+import com.parking.dashboard.R;
+import com.parking.dashboard.activity.DashboardActivity;
 
 public class ParkingSpotAndPaymentInformation extends Activity{
 
+   private ParkingBillingService mParkingBillingService = new ParkingBillingService();
+   int billingSupported = 0;
+   
    @Override
    protected void onCreate(Bundle savedInstanceState) {
-      // TODO Auto-generated method stub
+      
       super.onCreate(savedInstanceState);
       setContentView(R.layout.pspot_and_payment_info);
       Intent starterIntent = getIntent();
@@ -21,6 +31,26 @@ public class ParkingSpotAndPaymentInformation extends Activity{
       TextView textAll = (TextView) findViewById(R.id.textViewAll);
       textAll.setText(all);
       
+      //Set up connection to the service
+      mParkingBillingService.setContext(DashboardActivity.myContext);
+      
+      
+      Button payButton = (Button) findViewById(R.id.payButton);
+      payButton.setOnClickListener(new OnClickListener(){
+         @Override
+         public void onClick(View arg0) {
+            billingSupported = mParkingBillingService.isInAppBillingSupported();
+            if(billingSupported == ResponseCode.RESULT_OK.ordinal())
+            {
+               Toast.makeText(DashboardActivity.myContext, "SUPPORTED YAY", Toast.LENGTH_SHORT).show();
+               
+            }
+            else
+            {
+               Toast.makeText(DashboardActivity.myContext, "NAY :(", Toast.LENGTH_SHORT).show();
+            }
+         }
+      });
       
    }
 

b4f223b635ef74e6e170f0a0bfa1404bea8097a6
Vivek Gandhi <vm_gandhi@hotmail.com>
2012-04-09 21:18:24 -0700

    Remove unwanted logging

--- src/com/parking/rulesandregulations/RulesandRegulations.java
+++ src/com/parking/rulesandregulations/RulesandRegulations.java
@@ -11,19 +11,15 @@ import com.parking.dashboard.activity.DashboardActivity;
 
 public class RulesandRegulations extends Activity {
     
-	private static final String TAG = RulesandRegulations.class.getSimpleName();
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-		Log.v(TAG, "About to start webview");
+		
 		WebView mWebView;
     	super.onCreate(savedInstanceState);
-		Log.v(TAG, "Done with saved instance");
     	setContentView(R.layout.rulesandregulations);
-    	Log.v(TAG, "Done with setcontentview");
         mWebView = (WebView) findViewById(R.id.webview1);
-        Log.v(TAG, "Done with findviewbyid");
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.loadUrl("http://www.sandiego.gov/parking/enforcement/");
-        Log.v(TAG, "Done with loadurl");
     }
 }

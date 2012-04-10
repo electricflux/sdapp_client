f4e0584dc0398437673b54d1cd5630715e8f8294
Mandar Mulherkar <mandar.mulherkar@gmail.com>
2012-04-09 21:48:08 -0700

    temp parking spot LIST implementation

--- src/com/parking/findparking/FindParkingList.java
+++ src/com/parking/findparking/FindParkingList.java
@@ -1,36 +1,49 @@
-/*
- * Copyright © 2011 QUALCOMM Incorporated. All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * QUALCOMM Incorporated ("Proprietary Information"). You shall not
- * disclose such Proprietary Information, and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with QUALCOMM Incorporated.
- */
 package com.parking.findparking;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
+import android.app.SearchManager.OnCancelListener;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ListView;
 
+import com.google.android.maps.GeoPoint;
 import com.parking.dashboard.R;
+import com.parking.dashboard.activity.DashboardActivity;
 import com.parking.datamanager.ParkingLocationDataEntry;
+import com.parking.dbManager.DataBaseHelper;
 
-public class FindParkingList extends Activity{
+public class FindParkingList extends Activity implements OnClickListener{
    
    private ArrayList<ParkingLocationDataEntry> parkingData = new ArrayList<ParkingLocationDataEntry>(); 
    private MyParkingArrayAdapter aa = null;
+   
+   class updateList extends AsyncTask<Context, Integer, Integer>{
+
+      @Override
+      protected Integer doInBackground(Context... arg0) {
+         // TODO Auto-generated method stub
+         return null;
+      }
+      
+   }
    
    public void onCreate(Bundle savedInstanceState) {
       
       super.onCreate(savedInstanceState);
       setContentView(R.layout.parkingspotslistview);
    
-      ParkingLocationDataEntry test = new ParkingLocationDataEntry();
-      test.setMeterID(100L);
-      parkingData.add(test);
+      
+      DataBaseHelper myDbHelper = new DataBaseHelper(DashboardActivity.myContext);
+      myDbHelper.openDataBase(); 
+      //temp dummy
+      GeoPoint gp = new GeoPoint(0, 0);
+      myDbHelper.dbquery(gp, parkingData);
       
       ListView psListView = (ListView) findViewById(R.id.pSlistView);
       int resID = R.layout.pspotlistrowlayout;
@@ -39,5 +52,11 @@ public class FindParkingList extends Activity{
       
       
   }
+
+   @Override
+   public void onClick(View arg0) {
+      // TODO Auto-generated method stub
+      
+   }
 
 }

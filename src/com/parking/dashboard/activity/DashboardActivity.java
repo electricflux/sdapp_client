27ec82cc04475145d5c681cb65f6eb7b60721d24
unknown <prasanaa@PRASANAA.na.qualcomm.com>
2012-04-10 21:54:09 -0700

    Chintan CL

--- src/com/parking/dashboard/activity/DashboardActivity.java
+++ src/com/parking/dashboard/activity/DashboardActivity.java
@@ -98,7 +98,17 @@ public class DashboardActivity extends Activity{
 			case R.id.dashboard_button_personalbests:
 				i = new Intent(DashboardActivity.this, PaymentHistory.class);
 				break;
-
+				//Added to locate my car
+			case R.id.dashboard_button_viewall:
+			//i = new Intent(DashboardActivity.this, LocateMyCar.class);
+			//TODO: Provide lat lon from SharedPreference
+			double lat = 32.71283;
+			double lon = -117.165695;
+			Intent intent = new Intent(android.content.Intent.ACTION_VIEW, 
+			Uri.parse("http://maps.google.com/maps?daddr="+lat+","+lon+""));
+			intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+			startActivity(intent);
+			break;
 			case R.id.dashboard_button_parkandremind:
 				Toast.makeText(
 						DashboardActivity.this, "Functionality coming soon" , Toast.LENGTH_SHORT);
--- src/com/parking/findparking/FindParkingMap.java
+++ src/com/parking/findparking/FindParkingMap.java
@@ -61,8 +61,8 @@ public class FindParkingMap extends MapActivity{
 		myContext = getBaseContext();
 
 		pLocationManger = new ParkingLocationManager(getApplicationContext());
-		
-		
+
+
 		setUpLocationManager();
 		setUpMapView();
 		setUpMapController();
@@ -71,9 +71,9 @@ public class FindParkingMap extends MapActivity{
 
 	@SuppressWarnings({ "null", "unchecked" })
 	private void overlayParkingSpots() {
-		
+
 		//Async activity to get the parking stops from db
-		
+
 		List<ParkingLocationDataEntry> nLocList = new LinkedList<ParkingLocationDataEntry>();
 		ParkingLocationDataEntry nLoc = new ParkingLocationDataEntry();
 		nLoc.setLatitude((float)32.71283);
@@ -97,27 +97,27 @@ public class FindParkingMap extends MapActivity{
 		Log.v(TAG, "here ..4");
 		for (ParkingLocationDataEntry parkingSpot : FindParkingTabs.parkingLocations) {
 
-			
+
 			pSpotGeoPoint = parkingSpot.getGeoPoint();
 			address = "empty";//convertPointToLocation(pSpotGeoPoint);
 			sPpotInfo = LocationUtility.convertObjToString(parkingSpot);
 			//sPpotInfo = "empty string";
 			overlayitem = new OverlayItem(pSpotGeoPoint, address, sPpotInfo);
 			itemizedOverlays.addOverlay(overlayitem);
-		}	
-			mapView.getOverlays().add(itemizedOverlays);
-	//		Toast.makeText(getBaseContext(), address, Toast.LENGTH_SHORT).show();
-	 		mapView.postInvalidate();
-			
-		
+		}
+		mapView.getOverlays().add(itemizedOverlays);
+		// Toast.makeText(getBaseContext(), address, Toast.LENGTH_SHORT).show();
+		mapView.postInvalidate();
+
+
 	}
 
-//	private String convertObjToString(ParkingLocationDataEntry parkingSpot) {
-//		String pSpotInfo = Float.toString(parkingSpot.getLatitude()) + ","
-//				+ Float.toString(parkingSpot.getLongitude()) + ","
-//				+ parkingSpot.getMeterID();
-//		return pSpotInfo;
-//	}
+	// private String convertObjToString(ParkingLocationDataEntry parkingSpot) {
+	// String pSpotInfo = Float.toString(parkingSpot.getLatitude()) + ","
+	// + Float.toString(parkingSpot.getLongitude()) + ","
+	// + parkingSpot.getMeterID();
+	// return pSpotInfo;
+	// }
 
 	private void updateCurrentUserLocation() {
 
@@ -193,14 +193,14 @@ public class FindParkingMap extends MapActivity{
 		// parkingSpotsCursor.requery();
 		updateCurrentUserLocation();
 		overlayParkingSpots();
-		
+
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
 		// parkingSpotsCursor.deactivate();
-		
+
 	}
 
 	private static void createMarker() {
@@ -221,7 +221,7 @@ public class FindParkingMap extends MapActivity{
 		String address = "";
 		Geocoder geoCoder = new Geocoder(myContext, Locale.getDefault());
 		return LocationUtility.ConvertPointToLocation(point, geoCoder);
-	//	return address;
+		// return address;
 	}
 
 	@Override
@@ -240,7 +240,7 @@ public class FindParkingMap extends MapActivity{
 	private static void showNearbyParkingSpots(Location currentNewLocation) {
 
 		parkingSpotsVector = DBInterface
-				.getNearByParkingSpots(currentNewLocation);
+		.getNearByParkingSpots(currentNewLocation);
 
 	}
 
@@ -261,49 +261,49 @@ public class FindParkingMap extends MapActivity{
 		protected Boolean doInBackground(
 				List<ParkingLocationDataEntry>... arg0) {
 			Log.v( TAG, "doInBackground()" );
-			
+
 			/** Add your data */
 			List<ParkingLocationDataEntry> nLocList = arg0[0];
 			ParkingLocationDataEntry nLoc;
-			
+
 			Log.v( TAG, "doInBackground()" + nLocList.size());
 			nLoc = nLocList.get(0);
-			
+
 			DataBaseHelper myDbHelper = new DataBaseHelper(myContext);
 			//FindParkingTabs.parkingLocations = mParkingLocationsAll.getParkingLocations(2, 200, (float)32.71283, (float)-117.165695, myDbHelper);
 			FindParkingTabs.parkingLocations = mParkingLocationsAll.getParkingLocations(2, 200, (float)nLoc.getLatitude(), (float)nLoc.getLongitude(), myDbHelper);
 			Log.v(TAG, "NULLLLL" + FindParkingTabs.parkingLocations.size());
-		
+
 			overlayTappableParkingSpots();
 			return true;
 		}
-		
+
 		// -- gets called just before thread begins
-        @Override
-        protected void onPreExecute() 
-        {
-                Log.v( TAG, "onPreExecute()" );
-                super.onPreExecute();
-                
-        }
-       
-        // -- called if the cancel button is pressed
-        @Override
-        protected void onCancelled()
-        {
-                super.onCancelled();
-                Log.i( TAG, "onCancelled()" );
-        }
+		@Override
+		protected void onPreExecute()
+		{
+			Log.v( TAG, "onPreExecute()" );
+			super.onPreExecute();
 
-//        // -- called as soon as doInBackground method completes
-//        // -- notice that the third param gets passed to this method
-//        @Override
-//        protected void onPostExecute( String result ) 
-//        {
-//                super.onPostExecute(result);
-//                Log.v( TAG, "onPostExecute(): " + result );
-//        }
+		}
 
-		
+		// -- called if the cancel button is pressed
+		@Override
+		protected void onCancelled()
+		{
+			super.onCancelled();
+			Log.i( TAG, "onCancelled()" );
+		}
+
+		// // -- called as soon as doInBackground method completes
+		// // -- notice that the third param gets passed to this method
+		// @Override
+		// protected void onPostExecute( String result )
+		// {
+		// super.onPostExecute(result);
+		// Log.v( TAG, "onPostExecute(): " + result );
+		// }
+
+
 	}
 }

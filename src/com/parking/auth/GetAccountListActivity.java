7c7a28c4108f1cd17bf54b0f5fe1bbbc5b047988
Vivek Gandhi <vm_gandhi@hotmail.com>
2012-04-11 01:56:53 -0700

    Login UI simplified to display only email

--- src/com/parking/auth/GetAccountListActivity.java
+++ src/com/parking/auth/GetAccountListActivity.java
@@ -1,10 +1,13 @@
 package com.parking.auth;
 
+import java.util.ArrayList;
+
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +20,7 @@ import com.parking.dashboard.R;
 public class GetAccountListActivity extends ListActivity{
 	protected AccountManager accountManager;
 	protected Intent intent;
-	
+	private Account[] accounts;
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,15 +33,21 @@ public class GetAccountListActivity extends ListActivity{
         View header = inflater.inflate(R.layout.account_list_header_fragment, (ViewGroup) findViewById(R.id.header_layout_root));
         lv.addHeaderView(header, null, false);
  
+        
         accountManager = AccountManager.get(getApplicationContext());
-		Account[] accounts = accountManager.getAccountsByType("com.google");
-		this.setListAdapter(new ArrayAdapter(this, R.layout.listitem, accounts)); 
+		accounts = accountManager.getAccountsByType("com.google");
+		ArrayList<String> accountNameArray = new ArrayList<String>(accounts.length);
+		for(int i=0; i<accounts.length; i++)
+		{
+			accountNameArray.add(accounts[i].name);
+		}
+		this.setListAdapter(new ArrayAdapter(this, R.layout.listitem, accountNameArray)); 
     }
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-		Account account = (Account)getListView().getItemAtPosition(position);
-		ParkingApplication.setAccount(account);
+		//We do position -1 to get the position in the array (array starts from zero).
+		ParkingApplication.setAccount(accounts[position-1]);
 		Intent intent = new Intent(this, GetAuthTokenActivity.class);
 		startActivity(intent);
 		finish();
